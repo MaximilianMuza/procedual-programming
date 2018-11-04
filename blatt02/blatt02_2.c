133d56fa7b394ca57a053f78de1192403a87fb55
Max <maximilian.muza@gmx.de>
2018-11-04 11:43:00 +0100

    blatt02_2

--- blatt02/blatt02_2.c
+++ blatt02/blatt02_2.c
@@ -3,12 +3,66 @@
 #include <stdint.h>
 
 int main() {
-	
-	char num_1[20];
-    printf("Enter number: ");
-    scanf("%s", num_1);
-    printf("The binary number of %s is: %s.", num_1, bin_num_1);
-    
-    return 0;
+
+	// Definiere Variable i
+	// i entspricht der Bitlänge von uint16_t
+	int i = 16, val;
+	// Definiere Variable eingabe_nummer
+	uint16_t eingabe_nummer; 
+
+	// Gebe Text aus und frage nach Wert für eingabe_nummer
+	printf("Bitte gebe eine Zahl ein: ");
+	// Erhalte eingegebenen Wert von eingabe_nummer
+    scanf("%hd", &eingabe_nummer);
+
+    printf("\n");
+
+ 	// Iteriere über i bis es den Wert null hat
+ 	// Iteriere durch die verschiedenen Bits und gebe das Ergebnis aus
+  	while(i >= 0) {
+
+  		// Verschiebe die Binärzahl eingabe_nummer um den Wert i 
+  		// Überprüfe mit "& 1" den Wert von eingabe_nummer
+  		// Wenn das Ergebnis gleich zu eins ist, ist val true
+  		val = ((eingabe_nummer >> i) & 1) == 1;
+  		// Für den Fall das val true ist, wird die Zahl 1 als String ausgegeben
+  		// Sonst wird die Zahl 0 als String ausgegeben
+  		putchar( val ? '1' : '0');
+
+  		// Reduziere i um eins für die nächste Iteration
+  		i--;
+
+  	}
+
+  	printf("\n");
+
+	// uint16_t num, exponent, n, r;
+	// char binary_num[17];
+	// exponent = 16;
+
+ //    printf("Bitte gebe eine Zahl ein: \n");
+ //    scanf("%hd", &num);
+
+	// while(exponent >= 0) {
+	   	
+	//    	n = num/2(exponent);
+	//    	r = num%2(exponent);
+
+	//    	if(n > 2) {
+	   	
+	//    		r = r + (n - 1)*2(exponent);
+	//    		n = 1;
+
+	//    	}
+
+	//    	binary_num[exponent - 16] = n;
+
+	//     exponent = exponent - 1;
+	// }
+
+	// printf( "Das ist die Zahl: %hd\n", num);
+	// printf( "Und das ist sie in Binärschreibweise: %s\n", binary_num);
+
+   	return 0;
 
 }
--- blatt02/blatt02_3.c
+++ blatt02/blatt02_3.c
@@ -16,15 +16,18 @@ int main() {
 	// Der Wert von H iteriert bis er der Anzahl der vertikalen Kästchen entspricht
 	while(H < 2*N) {
 
-		// 
+		// Initialisierung von n für spätere Rechenoperationen 
+		// Für die erste und letzte Kästchen-Reihe gilt 
 		if(H == (N - 2) || H == (N + 2)) {
 
 			n = N - 2;
 
+		// Für die zweite und vorletzte Kästchen-Reihe gilt
 		} else if(H == (N - 1) || H == (N + 1)) {
 
 			n = N - 1;
 
+		// Für die mittlere Kästchen-Reihe gilt
 		} else if(H == N) {
 
 			n = N;
@@ -34,6 +37,7 @@ int main() {
 		// Der Wert von B iteriert bis er der Anzahl der horizontalen Kästchen entspricht
 		while(B < 2*N) {
 
+			// Für das erste und letzte Kästchen der Reihe H ist der Wert wie folgt
 			if (B == (N - 2) || B == (N + 2)) {
 
 				r = n - 2;
@@ -44,37 +48,41 @@ int main() {
 
 				}
 
+			// Für das zweite und vorletzte Kästchen der Reihe H ist der Wert wie folgt
 			} else if(B == (N - 1) || B == (N + 1)) {
 
 				r = n - 1;
 		
+			// Für das mittlere Kästchen der Reihe H ist der Wert wie folgt 
 			} else if(B == N) {
 
 				r = n;
 			}
 
+			// Ausgeben des Wertes von r in dem Kästchen bestimmt durch H und B
 			printf("%d", r);
 
+			// Setzten eines Linienbruchs (Breaks) wenn Ende der Reihe erreicht ist
 			if(B == (N + 2)) {
 
 				printf("\n");
 			
 			}
 
+			// Erhöhen von B um eins und Wechseln ins nächste Kästchen der Reihe
 			B = B + 1;
 
 		}
-		// Ausgeben der Werte in horizontaler Richtung
-		// printf("%d %d %d %d %d\n",a,b,c,b,a);
 
 		// Die Variable H wird um eins erhöht, um somit die nächste Reihe zu betrachten
 		H = H + 1;
 
-		// Der Wert B wird auf 1 zurückgesetzt
+		// Der Wert B wird für die nächste Reihe auf 1 zurückgesetzt 
 		B = 1;
 
 	}
 
+	// Ausgeben einer Leerzeile
 	printf("\n");
 
   	return 0;
