447984de241da36b553c5e863da997f353714ce5
Max <maximilian.muza@gmx.de>
2018-11-05 15:47:51 +0100

    done blatt02

--- blatt02/blatt02_3.c
+++ blatt02/blatt02_3.c
@@ -6,35 +6,57 @@ int main() {
 	
 	// Variablen werden definiert und anschließend Werte zugewiesen
 	int N, H, B, h, b, dH, dB;
-	N = 3;
 	H = B = 1;
-	// b = c - 1;
-	// a = b - 1;
+
+	// Gebe Text aus und frage nach Wert für N
+	printf("Bitte gebe eine Zahl zwischen 0 und 10 ein: ");
+	// Erhalte eingegebenen Wert von N
+    scanf("%d", &N);
 
 	printf("Programm zur Ausgabe einer Zahlen Raute\n\n");
 
 	// Der Wert von H iteriert bis er der Anzahl der vertikalen Kästchen entspricht
 	while(H < 2*N) {
 		
+		// Berechnen der Differenz von N und H
 		dH = N - H; 
+
+		// Nehme den Betrag von dH
 		if(dH < 0) {
+			
 			dH = -dH;
+		
 		}
+
+		// Berechnen der Differenz
 		h = N - dH;
 
 		// Der Wert von B iteriert bis er der Anzahl der horizontalen Kästchen entspricht
 		while(B < 2*N) {
 
+			// Berechnen der Differenz von N und B 
 			dB = N - B;
+			
+			// Nehme den Betrag von dB
 			if(dB < 0) {
+			
 				dB = -dB;
+			
 			}
+			
+			// Berechne Wert des Kästchens in Reihe H und Spalte B
 			b = h - dB;
 
+			// Ausgeben der Werte
+			// Ausnahme für Werte kleiner null 
 			if(b <= 0) {
+			
 				printf(" ");
+			
 			} else {
+			
 				printf("%d", b);
+			
 			}
 
 			// Erhöhen von B um eins und Wechseln ins nächste Kästchen der Reihe
