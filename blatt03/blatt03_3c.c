afd8768e8f0faf34b2ad5144fb33a406cdffbaf1
Max <maximilian.muza@gmx.de>
2018-11-15 15:26:03 +0100

    blatt03

--- blatt03/blatt03_3c.c
+++ blatt03/blatt03_3c.c
@@ -5,51 +5,27 @@ int main() {
 	
 	unsigned int  a = 200, b = 3, product, remainder;
 	int i = 1;
-	
-	// do {
 
-	// 	product = b * i;
-	// 	i++;
+	do {
+		do {
+			product = b * i;
+			i++;
+		} while(product <= a);
 
-	// } while(product <= a);
+		remainder = a - (product - b);
+		a = b;
+		b = remainder;
 
-	// remainder = a - (product - b);
+	} while(b != 0);
 
-	// do {
-
-	// 	remainder = a % b;
-	// 	a = b;
-	// 	b = remainder;
-
-	// } while(b != 0);
-
-	printf("%d\n", remainder);
+	printf("%d\n", a);
 	return 0;
 }
 
 
-
-
-// #include <stdio.h>
-// #include <stdint.h>
-
-// int main() {
-	
-// 	unsigned int  a = 200, b = 3, product, remainder;
-// 	int i = 1;
-
-// 	do {
 // 		do {
 // 			product = b * i;
 // 			i++;
 // 		} while(product <= a);
 		
 // 		remainder = a - (product - b);
-// 		a = b;
-// 		b = remainder;
-
-// 	} while(b != 0);
-
-// 	printf("%d\n", a);
-// 	return 0;
-// }
